a871b765824c4eec8248ef4bded4e6c223542d4e
marksio <15073448@imail.sunway.edu.my>
2019-10-20 12:50:39 +0800

    Fix counter issue on backup

--- backup.sh
+++ backup.sh
@@ -4,7 +4,6 @@
 logfileName="logfile_backup.txt"
 logfileNameForEvaluate="logfile_backup_evaluate.txt"
 dirBackupConfigFile="backup_config_file"
-counter=0
 
 function datetime() {
     echo `date +"%d-%m-%Y_%H:%M:%S:%N"`
@@ -159,8 +158,16 @@ function backupFile() {
         printf "Backup total of '$(countSourceFile)' file(s) from '${source}' to '${dest}' Successfully.\n"
         printf "$(datetime) - Backup total of '$(countSourceFile)' file(s) from '${source}' to '${dest}' Successfully.\n" >> $logfileName
         printf "~~~ Summary of Number of File(s) or Folder(s) Backed Up ~~~\n$(countFile)\n\n" >> $logfileName
-        counter+=1
-        printf "$counter $(datetime) ${source} ${dest} ${destTimestamp} \n" >> $logfileNameForEvaluate
+        if [ ! -f "$logfileNameForEvaluate" ]; then
+            let counter=1
+            printf "$counter $(datetime) ${source} ${dest} ${destTimestamp} \n" >> $logfileNameForEvaluate
+        else 
+            while read co da s de deTime; do
+                counter=$co
+            done < $logfileNameForEvaluate
+            let counter=counter+1
+            printf "$counter $(datetime) ${source} ${dest} ${destTimestamp} \n" >> $logfileNameForEvaluate
+        fi        
     else
         echo "There is [ERROR] backing up files from '${source}' to '${dest}'. Check you destination path.
         "
